42e4622714db64028b10eb7b88e25c12ac20191b
m7md3omer <mohammedomer789@gmail.com>
2019-03-16 05:41:06 +0200

    started implementing details acitivity

---
+++ app/src/main/java/com/moahammedomer/networkingliberaries/ProductDetailActivity.java
@@ -0,0 +1,13 @@
+package com.moahammedomer.networkingliberaries;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProductDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_detail);
+    }
+}
