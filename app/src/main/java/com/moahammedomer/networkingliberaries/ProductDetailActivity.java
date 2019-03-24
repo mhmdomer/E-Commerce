90aa3bb485ef4ef0261eac2396ab9f5870b9f4e0
m7md3omer <mohammedomer789@gmail.com>
2019-03-25 01:44:18 +0200

    added zooming functionality to product image

--- app/src/main/java/com/moahammedomer/networkingliberaries/ProductDetailActivity.java
+++ app/src/main/java/com/moahammedomer/networkingliberaries/ProductDetailActivity.java
@@ -1,25 +1,34 @@
 package com.moahammedomer.networkingliberaries;
 
+import android.app.Activity;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.github.chrisbanes.photoview.PhotoView;
 
 public class ProductDetailActivity extends AppCompatActivity {
 
     TextView price, description, country, category;
     ImageView image;
     Button order;
-    Dialog imageDialog;
     Toolbar toolbar;
     TextView title;
+    Activity activity;
+    Context context;
+    Bitmap bitmap;
+    public static int width = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +36,11 @@ public class ProductDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_product_detail);
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        activity = this;
+        if (width == -1){
+            width = findViewById(R.id.detail_container).getWidth();
+        }
+        context = this;
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
@@ -49,13 +63,14 @@ public class ProductDetailActivity extends AppCompatActivity {
         image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Dialog d = new Dialog(ProductDetailActivity.this);
-                d.setContentView(R.layout.image_layout);
-                ImageView imageView = d.findViewById(R.id.image_preview);
-                Glide.with(ProductDetailActivity.this)
-                        .load(intent.getExtras().getString(AllFragment.IMAGE_EXTRA))
-                        .into(imageView);
-                d.show();
+                bitmap = ((BitmapDrawable)image.getDrawable()).getBitmap();
+                final Dialog nagDialog = new Dialog(context,android.R.style.Theme_Translucent_NoTitleBar_Fullscreen);
+                nagDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                nagDialog.setCancelable(true);
+                nagDialog.setContentView(R.layout.image_layout);
+                PhotoView view = nagDialog.findViewById(R.id.image_preview);
+                view.setImageBitmap(bitmap);
+                nagDialog.show();
             }
         });
 
